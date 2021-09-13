549c7dc40d0398f4ea2a1d40a3579a052848dea3
captain <15012119@stu.nchu.edu.cn>
2021-09-13 15:26:54 +0800

    "修改启动文案"

--- src/main/java/com/captain/wiki/WikiApplication.java
+++ src/main/java/com/captain/wiki/WikiApplication.java
@@ -1,11 +1,13 @@
 package com.captain.wiki;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WikiApplication {
-
+    private static final Logger LOG = LoggerFactory.getLogger(WikiApplication.class);
     public static void main(String[] args) {
         SpringApplication.run(WikiApplication.class, args);
     }
