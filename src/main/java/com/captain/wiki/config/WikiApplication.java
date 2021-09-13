13e17efdf68cacb5a800efd9cf2db1119094989c
captain <15012119@stu.nchu.edu.cn>
2021-09-13 15:51:41 +0800

    "开发helloword接口"

--- src/main/java/com/captain/wiki/config/WikiApplication.java
+++ src/main/java/com/captain/wiki/config/WikiApplication.java
@@ -1,10 +1,13 @@
-package com.captain.wiki;
+package com.captain.wiki.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
 
+@ComponentScan("com.captain")
 @SpringBootApplication
 public class WikiApplication {
     private static final Logger LOG = LoggerFactory.getLogger(WikiApplication.class);
