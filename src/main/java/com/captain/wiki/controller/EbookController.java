3289e69a3f6c4b4dcdda6e88e70f5f3c1ad4a958
captain <15012119@stu.nchu.edu.cn>
2021-09-14 09:24:38 +0800

    增加了commonsresp类,以后所有的接口返回值都为commentsresp类型

--- src/main/java/com/captain/wiki/controller/EbookController.java
+++ src/main/java/com/captain/wiki/controller/EbookController.java
@@ -2,6 +2,7 @@ package com.captain.wiki.controller;
 
 
 import com.captain.wiki.domain.Ebook;
+import com.captain.wiki.resp.CommonResp;
 import com.captain.wiki.service.EbookService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,7 +18,11 @@ public class EbookController {
     private EbookService ebookService;
 
     @RequestMapping("/list")
-    public List<Ebook> list(){
-        return ebookService.list();
+    public CommonResp list(){
+
+        CommonResp<List<Ebook>> resp = new CommonResp<>();
+        List<Ebook> list = ebookService.list();
+        resp.setContent(list);
+        return resp;
     }
 }
