f75e6fa2fd36b30c941650496b168d2ef2a733e9
captain <15012119@stu.nchu.edu.cn>
2021-09-14 10:03:53 +0800

    制作copyUtils

--- src/main/java/com/captain/wiki/service/EbookService.java
+++ src/main/java/com/captain/wiki/service/EbookService.java
@@ -6,11 +6,10 @@ import com.captain.wiki.domain.EbookExample;
 import com.captain.wiki.mapper.EbookMapper;
 import com.captain.wiki.req.EbookReq;
 import com.captain.wiki.resp.EbookResp;
-import org.springframework.beans.BeanUtils;
+import com.captain.wiki.utils.CopyUtil;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -25,13 +24,16 @@ public class EbookService {
         criteria.andNameLike("%"+req.getName()+"%");
 
         List<Ebook> ebooks = ebookMapper.selectByExample(ebookExample);
-        List<EbookResp> respList = new ArrayList<>();
-
-        for (Ebook ebook : ebooks) {
-            EbookResp ebookResp = new EbookResp();
-            BeanUtils.copyProperties(ebook,ebookResp);
-            respList.add(ebookResp);
-        }
+//        List<EbookResp> respList = new ArrayList<>();
+//
+//        for (Ebook ebook : ebooks) {
+        //对象复制
+////            EbookResp ebookResp = new EbookResp();
+////            BeanUtils.copyProperties(ebook,ebookResp);
+//            EbookResp ebookResp = CopyUtil.copy(ebook,EbookResp.class);
+//            respList.add(ebookResp);
+//        }
+        List<EbookResp> respList = CopyUtil.copyList(ebooks,EbookResp.class);
 
         return respList;
 
